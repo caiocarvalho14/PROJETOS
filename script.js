c1cadb0465a3165e51014a4af51d4a38e7d90154
caiuslenda14 <k4io12345@gmail.com>
2025-04-25 21:34:03 -0300

    934

--- script.js
+++ script.js
@@ -86,3 +86,92 @@ carrossel.addEventListener("mouseleave", esconderBotoes);
 
 
 // MOBILE
+let paginaAtualM = 1;
+
+const paginasM = document.querySelectorAll(".mPage");
+const totalPaginasM = paginasM.length;
+
+const botoesProxM = document.querySelectorAll(".proxM");
+const botoesAntM = document.querySelectorAll(".antM");
+
+function mostrarPaginaMobile(num) {
+    paginasM.forEach(pagina => {
+        pagina.classList.remove("visible");
+        pagina.style.display = "none";
+    });
+
+    const atual = document.getElementById("pagem" + num);
+    if (atual) {
+        atual.style.display = "block";
+        atual.classList.add("visible");
+    }
+}
+
+window.onload = () => {
+    mostrarPaginaMobile(paginaAtualM);
+};
+
+botoesProxM.forEach(botao => {
+    botao.addEventListener("click", () => {
+        if (paginaAtualM < totalPaginasM) {
+            paginaAtualM++;
+            mostrarPaginaMobile(paginaAtualM);
+        }
+    });
+});
+
+botoesAntM.forEach(botao => {
+    botao.addEventListener("click", () => {
+        if (paginaAtualM > 1) {
+            paginaAtualM--;
+            mostrarPaginaMobile(paginaAtualM);
+        }
+    });
+});
+
+
+
+
+
+
+// carrossel mob
+let indiceAtualMobile = 0;
+
+function atualizarCarrosselMobile() {
+  const slidesMobile = document.getElementById('slidesMobile');
+  slidesMobile.style.transform = `translateX(-${indiceAtualMobile * 100}%)`;
+}
+
+function proximoMobile() {
+  const totalSlidesMobile = document.querySelectorAll('.slideMobile').length;
+  if (indiceAtualMobile < totalSlidesMobile - 1) {
+    indiceAtualMobile++;
+    atualizarCarrosselMobile();
+  }
+}
+
+function voltarMobile() {
+  if (indiceAtualMobile > 0) {
+    indiceAtualMobile--;
+    atualizarCarrosselMobile();
+  }
+}
+
+// MOSTRAR/ESCONDER BOTOES NO MOBILE
+let carrosselMobile = document.getElementById("carrosselMobile");
+let botoesCarrosselMobile = document.querySelectorAll(".botoesCarrosselProjectMobile");
+
+function mostrarBotoesMobile() {
+  botoesCarrosselMobile.forEach(botao => {
+    botao.style.animation = "fadeIn 0.5s forwards";
+  });
+}
+
+function esconderBotoesMobile() {
+  botoesCarrosselMobile.forEach(botao => {
+    botao.style.animation = "fadeOut 0.5s forwards";
+  });
+}
+
+carrosselMobile.addEventListener("mouseenter", mostrarBotoesMobile);
+carrosselMobile.addEventListener("mouseleave", esconderBotoesMobile);
