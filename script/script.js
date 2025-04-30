be8e73d5c102e4e3c49bbe59b1c61bb36d22242e
caiuslenda14 <k4io12345@gmail.com>
2025-04-29 23:57:01 -0300

    newAlteration

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
 document.getElementById('about-link').addEventListener('click', () => {
-    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+  document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
 });
 document.getElementById('habi-link').addEventListener('click', () => {
-    document.getElementById('habilities').scrollIntoView({ behavior: 'smooth' });
+  document.getElementById('habilities').scrollIntoView({ behavior: 'smooth' });
 });
 document.getElementById('projeto-link').addEventListener('click', () => {
-    document.getElementById('projetos').scrollIntoView({ behavior: 'smooth' });
+  document.getElementById('projetos').scrollIntoView({ behavior: 'smooth' });
 });
 document.getElementById('contato-link').addEventListener('click', () => {
-    document.getElementById('contato').scrollIntoView({ behavior: 'smooth' });
+  document.getElementById('contato').scrollIntoView({ behavior: 'smooth' });
 });
 const bandejas = document.querySelectorAll('.bandeja');
 
@@ -26,3 +26,41 @@ bandejas.forEach(bandeja => {
     bandeja.style.transform = 'perspective(1000px) rotateX(0deg) rotateY(0deg) scale(1)';
   });
 });
+
+const text = "Caio Carvalho.";
+    const typedTextSpan = document.getElementById("typed-text");
+    let index = 0;
+    let started = false;
+
+    function typeLetter() {
+      if (index < text.length) {
+        typedTextSpan.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeLetter, 50);
+      } else {
+        setTimeout(deleteLetter, 3000); 
+      }
+    }
+
+    function deleteLetter() {
+      if (index > 0) {
+        typedTextSpan.textContent = text.substring(0, index - 1);
+        index--;
+        setTimeout(deleteLetter, 100);
+      } else {
+        setTimeout(typeLetter, 500);
+      }
+    }
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && !started) {
+          started = true;
+          typeLetter();
+        }
+      });
+    }, {
+      threshold: 0.5
+    });
+
+    observer.observe(typedTextSpan);
